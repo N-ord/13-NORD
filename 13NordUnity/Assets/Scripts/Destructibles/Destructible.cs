995cd847a7848ab249d1a4b958c3e22d72f1399e
Sean <sean0262@gmail.com>
2019-10-24 10:02:41 +0200

    Destructibles added.

---
+++ 13NordUnity/Assets/Scripts/Destructibles/Destructible.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour { 
+   void OnCollisionEnter2D(Collision2D col) {
+        if(col.gameObject.tag == "Projectile") {
+            Destroy(gameObject);
+        }
+    }
+}
