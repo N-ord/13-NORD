773c590e1e95312f1a436eaec95ff78bbe31500b
lasse <lassefj0@gmail.com>
2019-10-24 10:03:36 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ 13NordUnity/Assets/ShotController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    [SerializeField]
+    bool MoveLeft;
+
+    [SerializeField]
+    float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveLeft) {
+
+        this.transform.position += Vector3.left * Speed * Time.deltaTime;
+        }
+        else {
+
+        this.transform.position += Vector3.right * Speed * Time.deltaTime;
+        }
+
+    }
+}
