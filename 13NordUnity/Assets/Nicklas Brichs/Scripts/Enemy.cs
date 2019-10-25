3abf9353ce7b145adc2a7dfa1dbb8a403d540999
Nicklas <nicklasbrichs@hotmail.com>
2019-10-25 09:35:42 +0200

    Enemy + Spawner

---
+++ 13NordUnity/Assets/Nicklas Brichs/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float movespeed;
+    float destroyTimer = 10f;
+    public Rigidbody enemyRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        movespeed = Random.Range(8f, 13f);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.velocity = new Vector3(-movespeed, enemyRb.velocity.y, enemyRb.velocity.z);
+
+
+        destroyTimer -= 1f * Time.deltaTime;
+        if (destroyTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag ==("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
---
+++ 13NordUnity/Assets/Nicklas Brichs/Scripts/Enemy_Spawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawn : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public float chooseRandomPos;
+    public float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = Random.Range(3f, 5f);
+        chooseRandomPos = Random.Range(-2f, 3.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= 1f * Time.deltaTime;
+        if (Timer <= 0f)
+        {
+            Timer = Random.Range(3f, 5f);
+            spawnEnemy();
+        }
+    }
+
+    void spawnEnemy()
+    {
+        chooseRandomPos = Random.Range(-2f, 3.5f);
+        Instantiate(enemyPrefab, new Vector3(9.5f, chooseRandomPos, 0f),Quaternion.identity);
+    }
+
+
+}
